9005a381f2798a927e2ffbe2b6515612f60b3209
Joel <Joel2.Richards@live.uwe.ac.uk>
2025-11-27 17:39:58 +0000

    tree change sizes

--- Assets/tree.cs
+++ Assets/tree.cs
@@ -15,14 +15,25 @@ public class tree : MonoBehaviour
     void Update(){
 
     }
-
-
-
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
+            Debug.Log("Enter");
             animator.SetTrigger("size down");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+
+        
+           
+
+        {
+            Debug.Log("Exit");
+            animator.SetTrigger("size up");
         }
     }
 
